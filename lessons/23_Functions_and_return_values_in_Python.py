7184e09e9741440dc7bf1b64647de2251b93564c
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-16 00:26:58 +0300

    master: 23 Functions and return values in Python

---
+++ lessons/23_Functions_and_return_values_in_Python.py
@@ -0,0 +1,26 @@
+def i_return():
+	return 5 + 5
+
+
+def i_print():
+	print(5 + 5)
+
+
+def i_five():
+	additing = 5 + 5
+	print(additing)
+	return additing
+
+
+# i_return()
+# i_print()
+# i_five()
+
+result_i_return = i_return()
+result_i_print = i_print()
+result_i_five = i_five()
+
+print(f'Result "result_i_return" is - {result_i_return}')
+print(f'Result "result_i_print" is - {result_i_print}')
+print(f'Result "result_i_five" is - {result_i_five}')
+
