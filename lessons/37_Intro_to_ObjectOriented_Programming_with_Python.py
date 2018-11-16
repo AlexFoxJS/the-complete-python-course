8b4dd1736d032ada4c14ee8eed70bfe3f6f160a5
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-16 12:28:56 +0300

    master: 38 More about classes and objects
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

--- lessons/37_Intro_to_ObjectOriented_Programming_with_Python.py
+++ lessons/37_Intro_to_ObjectOriented_Programming_with_Python.py
@@ -17,12 +17,12 @@ class Student:
 		self.name = name
 		self.grades = grades
 
-	def avarage(self):
+	def average(self):
 		return sum(self.grades) / len(self.grades)
 
 
 student_1 = Student('Alex Fox', [20, 40, 88, 90])
 student_2 = Student('Axel Xof', [50, 31, 68, 110])
 
-print(f"Name: {student_1.name}, Avarage: {student_1.avarage()}, Class: {student_1.__class__}")
-print(f"Name: {student_2.name}, Avarage: {student_2.avarage()}, Class: {student_2.__class__}")
+print(f"Name: {student_1.name}, Average: {student_1.average()}, Class: {student_1.__class__}")
+print(f"Name: {student_2.name}, Average: {student_2.average()}, Class: {student_2.__class__}")
