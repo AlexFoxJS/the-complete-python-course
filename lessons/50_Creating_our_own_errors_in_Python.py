7d4c2cb907e870762d8031dc838dd66e103742d7
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-19 16:46:20 +0300

    master: 50 Creating our own errors in Python
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

---
+++ lessons/50_Creating_our_own_errors_in_Python.py
@@ -0,0 +1,12 @@
+class RuntimeErrorWithCode(TypeError):
+	"""
+	Error description:
+	- Exception raised when a specific error code is needed.
+	"""
+	def __init__(self, message, code):
+		super().__init__(f'Error code {code}: {message}')
+		self.code = code
+
+
+err = RuntimeErrorWithCode('Some error message.', 500)
+print(err.__doc__)
