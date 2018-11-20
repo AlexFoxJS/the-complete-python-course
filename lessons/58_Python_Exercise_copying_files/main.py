1b31958dff87d430661ee09f7129a4148779de93
AlexFoxJS <alexfoxjs@gmail.com>
2018-11-21 00:41:48 +0300

    master: 58 transfer files in a separate folder
    Signed-off-by: AlexFoxJS <alexfoxjs@gmail.com>

--- lessons/58_Python_Exercise_copying_files/main.py
+++ lessons/58_Python_Exercise_copying_files/main.py
@@ -1,9 +1,9 @@
 # Ask the user for list of 3 frends
 # For each friend, we'll tell the user whether they are nearby
-# For each nearby friend, we'll save their name to '58_Python_Exercise_copying_files__people.txt'
+# For each nearby friend, we'll save their name to 'people.txt'
 
 friends = input('Enter three friends names, separate by commas (no space please): ').split(',')
-file = open('58_Python_Exercise_copying_files__people.txt', 'r')
+file = open('people.txt', 'r')
 people_nearby = [line.strip() for line in file.readlines()]
 file.close()
 
@@ -12,7 +12,7 @@ people_nearby_set = set(people_nearby)
 
 friends_nearby_set = friends_set.intersection(people_nearby_set)
 
-nearby_friends_file = open('58_Python_Exercise_copying_files__nearby_friends_file.txt', 'w')
+nearby_friends_file = open('nearby_friends_file.txt', 'w')
 
 for friend in friends_nearby_set:
 	print(f'{friend} is nearby! Meet up with them.')
